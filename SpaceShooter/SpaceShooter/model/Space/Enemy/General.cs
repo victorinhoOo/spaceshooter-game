b203aa615dcf596f3b06a03bb15332aca612269e
Tayk0oo <ttmdu89@gmail.com>
2023-06-06 17:07:48 +0200

    modif des enmies squeltte classe

--- SpaceShooter/SpaceShooter/model/Space/Enemy/General.cs
+++ SpaceShooter/SpaceShooter/model/Space/Enemy/General.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IUTGame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace SpaceShooter.model.Space.Enemy
 {
-    internal class General
+    public class General : Enemy
     {
+        public General(double x, double y, Game g, string name = "Ship_5.png") : base(x, y, g, name, -100)
+        {
+
+
+
+        }
+        public override string TypeName => "General";
     }
 }
