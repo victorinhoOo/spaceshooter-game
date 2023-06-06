5002fbcb48601138ff2cc9b2e0f383b16c2b4519
vd577076 <victor.duboz@iut-dijon.u-bourgogne.fr>
2023-06-06 14:20:36 +0200

    ajout sprites

--- SpaceShooter/SpaceShooter/view/MainWindow.xaml.cs
+++ SpaceShooter/SpaceShooter/view/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SpaceShooter
+namespace SpaceShooter.view
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
