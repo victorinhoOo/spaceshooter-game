2aadcf4b1174e9b2b57526310fc39c78aa7e58eb
Tayk0oo <ttmdu89@gmail.com>
2023-06-06 14:43:48 +0200

    plusieurs repertoire et ajout de generateur de balle pour l'adapter en generateur de vaisseaux mechant

--- SpaceShooter/SpaceShooter/model/Spaceship.cs
+++ SpaceShooter/SpaceShooter/model/Spaceship.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IUTGame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace SpaceShooter.model
 {
-    public class Spaceship
+    public class Spaceship : GameItem
     {
+
+
+
     }
 }
