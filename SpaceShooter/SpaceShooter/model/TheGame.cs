436b8197149119358d9500cbcb1f15a688a9d162
Tayk0oo <ttmdu89@gmail.com>
2023-06-12 13:38:26 +0200

    k

--- SpaceShooter/SpaceShooter/model/TheGame.cs
+++ SpaceShooter/SpaceShooter/model/TheGame.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using SpaceShooter.model.Vaisseaux.Enemi;
+using SpaceShooter.model.Space;
 
 namespace SpaceShooter.model
 {
